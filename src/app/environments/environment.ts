52daca89ea780bb69dbe4ff14f51e14eed0bc656
Marx Quiroz <marxquva@gmail.com>
2025-10-08 12:21:39 -0500

    endpoint

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
-  //apiUrl: 'https://apitorneoaniversario.onrender.com/api'
+  //apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://apitorneoaniversario.onrender.com/api'
   //https://apitorneoaniversario.onrender.com/api/equipos/2
 
 };
