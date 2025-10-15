1cbc0ef4219328b170e0154f5b80cfb4eee99d6f
Marx Quiroz <marxquva@gmail.com>
2025-10-15 00:26:24 -0500

    tabla posiciones

---
+++ src/app/interfaces/posicion.interface.ts
@@ -0,0 +1,13 @@
+export interface Posicion {
+  idequipo: number;
+  equipo: string;
+  logotipo: string;
+  partidos_jugados: number;
+  partidos_ganados: number;
+  partidos_empatados: number;
+  partidos_perdidos: number;
+  goles: number;
+  goles_en_contra: number;
+  diferencia_goles: number;
+  puntos: number;
+}
--- src/app/pages/admin/dashboard/statistics/components/tablaposiciones/tablaposiciones.component.ts
+++ src/app/pages/admin/dashboard/statistics/components/tablaposiciones/tablaposiciones.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit, computed, signal  } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
 import { SinDatosComponent } from "@components/shared/sin-datos/sin-datos.component";
+import { PosicionesService } from '@services/posiciones/posiciones.service';
+import { LoadingService } from '@services/shared/loading.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Posicion } from '@interfaces/posicion.interface';
 
-
+/*
 export interface productsData {
   id: number;
   imagePath: string;
@@ -76,16 +80,62 @@ const PRODUCT_DATA: productsData[] = [
   diferencia_goles: 3,
   puntos: 'critical'
 },
-];
+];*/
 
 
 @Component({
   selector: 'app-tablaposiciones',
-  imports: [MatTableModule, CommonModule, MatDividerModule, SinDatosComponent],
+  imports: [MatTableModule, CommonModule, MatDividerModule, SinDatosComponent, MatProgressSpinnerModule],
   templateUrl: './tablaposiciones.component.html',
   styleUrl: './tablaposiciones.component.scss'
 })
-export class TablaposicionesComponent {
-  	displayedColumns1: string[] = ['id','equipo', 'pj', 'pg', 'pe', 'pp','gf','gc','df','pts'];
-    dataSource1 = PRODUCT_DATA;
+export class TablaposicionesComponent implements OnInit {
+
+  displayedColumns1: string[] = ['id','equipo', 'pj', 'pg', 'pe', 'pp','gf','gc','df','pts'];
+  //dataSource1 = PRODUCT_DATA;
+
+  constructor(private loadingService: LoadingService, private posicionesService: PosicionesService) {}
+  isLoading = computed(() => this.loadingService.isLoading());
+  posiciones = signal<Posicion[]>([]);
+  //const PRODUCT_DATA: Posicion[] = []
+
+  ngOnInit(): void {
+    const edicionId = 2;
+    this.loadingService.setLoading(true);
+
+    this.posicionesService.getTablaPosicionById(edicionId).subscribe({
+      next: (res) => {
+        if (res) {
+          console.log(res)
+          this.posiciones.set(res);
+        } else {
+          this.posiciones.set([]);
+        }
+        this.loadingService.setLoading(false);
+
+      },
+      error: (error) => {
+        console.error('Error al cargar el fixture:', error);
+        this.loadingService.setLoading(false);
+      }
+    });
+  }
+
+  getPuntosClass(puntos: number): string {
+    switch(puntos) {
+      /*case 21:
+        return 'bg-light-secondary text-secondary';
+      case 18:
+        return 'bg-light-primary text-primary';
+      case 15:
+        return 'bg-light-warning text-warning';
+      case 0:
+        return 'bg-light-error text-error';
+      case 24:
+        return 'bg-light-success text-success';*/
+      default:
+        return '';
+    }
+  }
+
 }
