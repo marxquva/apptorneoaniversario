59040a45f8d7962cca8f54d0afa2128304938b4b
Marx <marxquva@gmail.com>
2026-04-21 17:42:16 -0500

    apiurl

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:3000/api'
-  apiUrl: 'ttps://apitorneoaniversario.onrender.com/api'
+  apiUrl: 'https://apitorneoaniversario.onrender.com/api'
   //https://apitorneoaniversario.onrender.com/api/equipos/2
 
 };
